a2fd756f19ec9e7bf56f9dd102101db1a0a1198f
ronia <roniamir24@gmail.com>
2025-12-30 14:01:07 +0200

    עדכון ל HOMEPAGECONTROLLER

--- src/HomePage/main/java/org/example/homepage/HomePageController.java
+++ src/HomePage/main/java/org/example/homepage/HomePageController.java
@@ -5,18 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import entities.User;
+import javafx.scene.control.Label;
 
 public class HomePageController {
 
     @FXML private AnchorPane centerHost;
     @FXML private Button loginBtn;
+    @FXML private Label lblWelcome; // ה-Label מתןך ה-SceneBuilder
+    private User currentUser = null; // משתנה חדש במקום ה-boolean loggedIn
 
     private boolean loggedIn = false;
 
     @FXML
     public void initialize() {
-        onLoginOrProfile();   // Login first
-        updateLoginButton();
+        /*onLoginOrProfile();   // Login first
+        //updateLoginButton();*/
+        onCatalog(); // ברירת מחדל: מציג את הקטלוג
+        updateUI();  // קורא לפונקציה החדשה שמסדרת את הכפתורים
     }
 
     @FXML
@@ -36,7 +42,12 @@ public class HomePageController {
 
     @FXML
     private void onLoginOrProfile() {
-        if (!loggedIn) {
+      /*  if (!loggedIn) {
+            showPage("/org/example/homepage/LoginPage.fxml");
+        } else {
+            showPage("/org/example/homepage/ProfilePage.fxml");
+        }*/
+        if (currentUser == null) {
             showPage("/org/example/homepage/LoginPage.fxml");
         } else {
             showPage("/org/example/homepage/ProfilePage.fxml");
@@ -61,6 +72,24 @@ public class HomePageController {
             centerHost.getChildren().setAll(view);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void setLoggedInUser(User user) {
+        this.currentUser = user;
+        updateUI(); // עדכון השם והכפתור
+        onCatalog(); // החזרה לקטלוג הראשי (אופציונלי)
+    }
+
+    private void updateUI() {
+        if (currentUser == null) {
+            // מצב אורח
+            loginBtn.setText("Login");
+            if (lblWelcome != null) lblWelcome.setText("Welcome, Guest");
+        } else {
+            // מצב מחובר - שימוש בשם האמיתי
+            loginBtn.setText("Profile");
+            if (lblWelcome != null) lblWelcome.setText("Welcome, " + currentUser.getFirstName());
         }
     }
 }
