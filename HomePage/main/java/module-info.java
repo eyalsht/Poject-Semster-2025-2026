d533144f8f5df49a69e39447c7edfc75995a24ae
Eyal Shtinmetz <eyalshtinmetz@gmail.com>
2025-12-30 14:14:04 +0200

    javfx

--- HomePage/main/java/module-info.java
+++ HomePage/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.example.homepage {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens org.example.homepage to javafx.fxml;
     exports org.example.homepage;
